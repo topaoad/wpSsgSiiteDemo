d8f5b5a16f7548499f4ad7117d17366082fb9e61
toproad <tattun0817@gmail.com>
2022-10-15 16:43:16 +0900

    🚑 ダイナミックルート取得失敗中

--- src/pages/[...slug] .tsx
+++ src/pages/[...slug] .tsx
@@ -3,19 +3,19 @@ import client from "src/lib/apollo/client";
 import { BlockRenderer } from "src/components/BlockRenderer";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { cleanAndTransformBlocks } from "src/utils/cleanAndTransformBlocks";
-// import { getPageStaticProps } from "src/utils/getPageStaticProps";
+import { getPageStaticProps } from "src/utils/getPageStaticProps";
 // import { Page } from "src/components/Page";
 
 export type DynamicData = {
   blocks: [];
 };
 
-export type getStaticPathsProps= {
+export type getStaticPathsProps = {
   uri: string;
-
 };
 
 export const Page: NextPage<DynamicData> = ({ blocks }) => {
+  console.log(blocks);
   return (
     <div>
       <BlockRenderer blocks={blocks} />
@@ -23,7 +23,7 @@ export const Page: NextPage<DynamicData> = ({ blocks }) => {
   );
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getStaticPaths = async () => {
   const { data } = await client.query({
     query: gql`
       query AllPagesQuery {
@@ -41,85 +41,31 @@ export const getStaticPaths: GetStaticPaths = async () => {
     `,
   });
 
+  // ★うまく取得できなかったので書き換えてみたが、それでもエラー
+  const paths = data.pages.nodes
+    .filter((page: { uri: string }) => page.uri !== "/")
+    .map((page: { uri: string }) => {
+      const pathsCm = page.uri.substring(1, page.uri.length - 1).split("/");
+      return pathsCm;
+    });
+
   return {
-    // paths: [...data.pages.nodes, ...data.properties.nodes]
-    paths: data.pages.nodes
-      .filter((page:any) => page.uri !== "/")
-      .map((page:any) => ({
-        params: {
-          // substring() メソッドは string オブジェクトの開始・終了位置の間、または文字列の最後までの部分集合を返します。
-          slug: page.uri.substring(1, page.uri.length - 1).split("/"),
-        },
-      })),
-    fallback: false,
+    //　★ここは解答例のとおり
+    // paths: data.pages.nodes
+    //   .filter((page: any) => page.uri !== "/")
+    //   .map((page: any) => ({
+    //     params: {
+    //       // substring() メソッドは string オブジェクトの開始・終了位置の間、または文字列の最後までの部分集合を返します。
+    //       slug: page.uri.substring(1, page.uri.length - 1).split("/"),
+    //     },
+    //   })),
+
+    //　★静的slugと動的slugを両方書いてもどちらも反応しない。
+    paths: [{ params: { slug: "contact" } }, { params: { slug: paths } }],
+    fallback: "blocking",
   };
 };
 
-// export const getStaticProps: GetStaticProps = getPageStaticProps;
-
-export const getPageStaticProps:GetStaticProps = async () => {
-  const { data } = await client.query({
-    query: gql`
-      query PageQuery {
-        nodeByUri(uri: "/ssg-relation-page") {
-          ... on Page {
-            id
-            blocksJSON
-            title
-          }
-        }
-        acfOptionsMainMenu {
-          mainMenu {
-            callToActionButton {
-              destination {
-                ... on Page {
-                  id
-                  uri
-                }
-              }
-              label
-            }
-            menuItems {
-              menuItem {
-                destination {
-                  ... on Page {
-                    id
-                    uri
-                  }
-                }
-                label
-              }
-              items {
-                destination {
-                  ... on Page {
-                    uri
-                    id
-                  }
-                }
-                label
-              }
-            }
-          }
-        }
-      }
-    `,
-  });
-
-  // awaitをつけないとエラーとなります。blocksにpromiseが返ってきます。
-  const blocks = await cleanAndTransformBlocks(data.nodeByUri.blocksJSON);
-
-  return {
-    props: {
-      data: data,
-      blocks: blocks,
-      mainMenuItems: data.acfOptionsMainMenu.mainMenu.menuItems,
-      callToActionLabel:
-        data.acfOptionsMainMenu.mainMenu.callToActionButton.label,
-      callToActionDestination:
-        data.acfOptionsMainMenu.mainMenu.callToActionButton.destination.uri,
-    },
-  };
-};
-
+export const getStaticProps: GetStaticProps = getPageStaticProps;
 
 export default Page;
---
+++ src/pages/buying.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import { ReactElement } from "react";
+
+const About= (): JSX.Element => {
+  return (
+    <div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+    </div>
+  );
+};
+
+export default About;
